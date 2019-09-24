65361a85bee624e6cfb6636bb1e9c7c5916452c2
Donovan Jones <donovanljones94@gmail.com>
2019-09-24 14:56:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,21 @@ class Artist
     Song.all.find_all {|song| song.artist == self}
   end
   
+  def self.find_or_create_by_name(name)
+    if @@all.any? {|artist| artist.name == name}
+      return @@all.find {|artist| artist.name == name}
+    else
+      new_artist = Artist.new(name)
+      return new_artist
+    end
+  end
   
+  def print_songs
+    song_array = Song.all.find_all {|song| song.artist == self}
+    song_array.each do |song|
+      puts song.name
+    end
+  end
   
 end 
     
